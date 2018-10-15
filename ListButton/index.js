1075a0bbb2ec5069293646b61a620043d18a33e8
Wesley Luna <wesley.luna@bellevuecollege.edu>
2018-10-15 10:28:36 -0700

    final workings for the list button

--- ListButton/index.js
+++ ListButton/index.js
@@ -1,6 +1,12 @@
 window.onload = () => {
+    const listButton = document.getElementById('add-to-list');    
     const userInput = document.getElementById('list-data');
     const setTextButton = document.getElementById('set-text');
+
+    listButton.onclick = () => {
+        const myList = document.getElementById('my-list');
+        elfCode.appendToList(myList, userInput.value);
+    }
 
     setTextButton.onclick = () => {
         userInput.value = 'The first item for my list';
