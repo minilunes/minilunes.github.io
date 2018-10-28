aaa631a4f87e4b6e57f4bb787a2785a59830f15f
EC2 Default User <ec2-user@ip-172-31-80-141.ec2.internal>
2018-10-28 20:32:44 +0000

    box model and aside work

--- ListButton/index.js
+++ ListButton/index.js
@@ -1,14 +1,14 @@
 window.onload = () => {
-    const listButton = document.getElementById('add-to-list');    
-    const userInput = document.getElementById('list-data');
+    const userInput = document.getElementById('list-data');    
     const setTextButton = document.getElementById('set-text');
-
-    listButton.onclick = () => {
-        const myList = document.getElementById('my-list');
-        elfCode.appendToList(myList, userInput.value);
-    }
+    const listButton = document.getElementById('add-to-list');
 
     setTextButton.onclick = () => {
         userInput.value = 'The first item for my list';
+     }
+
+     listButton.onclick = () => {
+        const myList = document.getElementById('my-list');
+        elfCode.appendToList(myList, userInput.value);
     }
 }
