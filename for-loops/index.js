999eed146b013e81d8d301cc1d18d83a731224d9
EC2 Default User <ec2-user@ip-172-31-80-141.ec2.internal>
2018-11-03 23:16:05 +0000

    committing for for-loops file

--- for-loops/index.js
+++ for-loops/index.js
@@ -1,7 +1,12 @@
 window.onload = () => {
     const listButton = document.getElementById('run-for-loop');
 
-    for (let i = 0; i < 5; i++) {
-        elfCode.appendToList(myList, i);
+    listButton.onclick = () => {
+        const myList = document.getElementById('my-list');
+        
+        for (let i = 0; i < 5; i++) {
+           elfCode.appendToList(myList, i);
+        }
+        
     }
-} 
+}
