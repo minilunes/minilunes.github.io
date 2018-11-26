375b07c59098decc37544e187fad4e916ac9f578
Wesley Luna <wesley.luna@bellevuecollege.edu>
2018-11-26 12:24:29 -0800

    committing for 11/26

--- javascript-test/javascript-arrays.js
+++ javascript-test/javascript-arrays.js
@@ -1,25 +1,27 @@
 window.onload = () => {
-    const numbersAction = document.getElementById('numbers-action');
-    const languagesAction = document.getElementById('languages-action')
-    
-    numbersAction.onclick = () => {
-        const numbersDisplay = document.getElementById('numbers-display');
-            numbersDisplay.textContent = numbers;
-    }
-    
-    languagesAction.onclick = () => {
-        const languagesDisplay = document.getElementById('languages-display')
-            languagesDisplay.textContent = languages;
-    }
-    
+
+const arrays = {
+    numbers: [15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5],
+    languages: ['JavaScript', 'HTML', 'CSS', 'Java', 'C#', 'Python', 'C/C++']
 };
 
-const numbers = [15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5];
-const languages = ['JavaScript', 'HTML', 'CSS', 'Java', 'C#', 'Python', 'C/C++'];
-
-for (let number of numbers) {
-    console.log(numbers);
+const numbersAction = document.getElementById('numbers-action');
+const languagesAction = document.getElementById('languages-action')
+    
+numbersAction.onclick = () => {
+    const numbersDisplay = document.getElementById('numbers-display');
+    for (let number of arrays.numbers) {
+        const li = document.createElement("li");
+        li.appendChild(document.createTextNode(number));
+        numbersDisplay.appendChild(li);
+    }
 }
-for (let language of languages) {
-    console.log(languages);
-}
+    
+languagesAction.onclick = () => {
+    const languagesDisplay = document.getElementById('languages-display')
+    for (let language of arrays.languages.sort()) {
+        const li = document.createElement("li");
+        li.appendChild(document.createTextNode(language))
+        languagesDisplay.appendChild(li);
+    }
+}}
