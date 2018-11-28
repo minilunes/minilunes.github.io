0c91804c2f8934636c3ab5ffb9dc7da67df0b345
EC2 Default User <ec2-user@ip-172-31-80-141.ec2.internal>
2018-11-28 01:11:03 +0000

    commit for understanding arrays

--- javascript-test/radio-choices.js
+++ javascript-test/radio-choices.js
@@ -1,16 +1,32 @@
 window.onload = () => {
-    const myForm = document.getElementById('sort-type');
+const myForm = document.getElementById('sort-type');
 
-    myForm.addEventListener('submit', (event) => {
-        event.preventDefault();
-        const value = document.querySelector('input[name="sorter"]:checked').value;
-        if (value.toLowerCase() === 'language') {
-            showChoice(value);
-        } else {
-            showChoice(value);
-        }
-    });
+myForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const value = document.querySelector('input[name="sorter"]:checked').value;
+    if (value.toLowerCase() === 'green') {
+        setColor("blueBackground");
+        showChoice(value);
+    }
+    else {
+        setColor("greenBackground");
+        showChoice(value);
+    }
+});
 }
+
+let currentClass = null;
+
+const setColor = (className) => {
+        console.log("SET CLASS TO", className);
+        var element = document.getElementById("form-section");
+        if (currentClass) {
+            element.classList.remove(currentClass);
+        }
+        currentClass = className;
+        element.classList.add(className);
+    }
+
 
 function showChoice(value) {
     console.log(value);
