ad3df0fe4c462fab5afb372f70def3f0190da6e0
minilunes <wesley.luna@bellevuecollege.edu>
2019-01-21 16:36:55 -0800

    haltime commit for exercise 5

---
+++ PROG209/Exercise 2/exercise2.js
@@ -0,0 +1,11 @@
+for (i = 1; i <= 100; i++) {
+    if (!(i % 5)) {
+        console.log('Fizz');
+    }
+    else if (!(i % 7)) {
+        console.log('Fuzz');
+    }
+    else {
+        console.log(i);
+    }
+}
---
+++ PROG209/Exercise 2/exercise2partB.js
@@ -0,0 +1,3 @@
+console.log(Math.min(100, 1000) + 100);
+
+console.log(min(-450, 1000));
