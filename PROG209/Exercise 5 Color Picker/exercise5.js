d29dee50f4868743516293ecdec961ba8725e277
minilunes <wesley.luna@bellevuecollege.edu>
2019-01-27 10:45:46 -0800

    commit for prog209 exercises

--- PROG209/Exercise 5 Color Picker/exercise5.js
+++ PROG209/Exercise 5 Color Picker/exercise5.js
@@ -5,9 +5,9 @@ showButton.addEventListener("click", showColorHandler, false);
 clearButton.addEventListener("click", clearColorHandler, false);
 
 function showColorHandler(e) {
-	var r = parseInt(input.value);
-	var g = parseInt(input.value);
-	var b = parseInt(input.value);
+	var r = parseInt(inputred.value);
+	var g = parseInt(inputgreen.value);
+	var b = parseInt(inputblue.value);
 
 	colorBar.style.backgroundColor  = "rgb(" + r + "," + g + "," + b + ")";			
 }
@@ -20,21 +20,3 @@ function clearColorHandler(e) {
 function clear() {
     colorBar.style.backgroundColor= "White";
 }
-
-// function validate(r) {
-//     if ( r >=256 || r < 1) {
-//     else {
-//             alert ("You must enter a number from 1-255");
-//     }
-// }
-// }
-// // function validate(g) {
-// //     if ( r >=256 || r < 1) {
-// //     alert ("You must enter a number from 1-255")
-// //     }
-// // }
-// // function validate(b) {
-// //     if ( r >255 || r < 1) {
-// //     alert ("You must enter a number from 1-255")
-// //     }
-// // }
